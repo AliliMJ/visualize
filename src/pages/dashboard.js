e5d14b4718038b8d14d7a6e1752a23015a3ea75b
Alili <alili.mohamedlamine@gmail.com>
2021-06-10 22:25:55 +0100

    Added dashboard component containing profile, and support sub-components and applyed routes on them.

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,7 +1,54 @@
 import Support from './support';
+import { Link, Switch, Route } from 'react-router-dom';
+import Profile from './profile';
+import { useAuth } from '../hook/useAuth';
+import logo from '../svg/logo.svg';
+
+const SideBar = (props) => {
+  const { logout } = useAuth();
+  const handleLogout = async () => {
+    try {
+      await logout();
+    } catch {
+      alert('Error:Could not logout.');
+    }
+  };
+  return (
+    <div className={`flex flex-col ${props.className}`}>
+      <Link to="/dashboard/profile">Profile</Link>
+      <Link to="/dashboard/support">Support</Link>
+      <button
+        className="text-red-500 rounded px-4 py-1 bg-transparent border-2 border-red-500"
+        onClick={handleLogout}
+      >
+        Log out
+      </button>
+    </div>
+  );
+};
+
+const Navbar = ({ className }) => {
+  return <div className={`h-20 flex ${className} bg-blue-300`}></div>;
+};
 
 const Dashboard = () => {
-  return <Support />;
+  return (
+    <div className="grid grid-cols-5 px-2 gap-1">
+      <img className="w-20 place-self-center" src={logo} />
+      <Navbar className="col-span-4 col-start-2" />
+      <SideBar className="px-10 py-5 row-start-2 row-span-4" />
+      <div className="col-start-2 col-span-4 row-span-4 row-start-2 border bg-white rounded">
+        <Switch>
+          <Route path="/dashboard/profile">
+            <Profile />
+          </Route>
+          <Route path="/dashboard/support">
+            <Support />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,83 +1,80 @@
-import { useEffect, useState } from 'react';
-import { database } from '../api/firebase';
 import { useAuth } from '../hook/useAuth';
 import { FaCamera } from 'react-icons/fa';
-
-const Profile = () => {
-  const { logout, user } = useAuth();
-  const [userInfo, setUserInfo] = useState({});
-
-  const handleLogout = async () => {
-    try {
-      await logout();
-    } catch {
-      alert('Error:Could not logout.');
-    }
-  };
-
-  useEffect(() => {
-    return database.users
-      .doc(user.uid)
-      .onSnapshot((doc) => setUserInfo({ ...doc.data() }));
-  }, [user.uid]);
-
+const Title = () => {
   return (
-    <div>
-      <button
-        className="text-red-500 rounded px-4 py-1 bg-transparent border-2 border-red-500"
-        onClick={handleLogout}
-      >
-        Log out
-      </button>
+    <div className="col-span-full space-y-2">
+      <h1 className="text-3xl">Données personnelles</h1>
 
-      <div className="flex rounded  py-6 border bg-white">
-        <div className="flex flex-col border-r px-10 h-max items-center w-min space-y-6">
-          <div className="relative">
-            <div className="rounded-full w-20 h-20 overflow-hidden flex items-center justify-center">
-              <img
-                className="object-cover h-full w-full"
-                src="https://source.unsplash.com/random"
-              />
-            </div>
-            <div className="absolute rounded-full w-8 h-8 bg-white flex items-center justify-center -bottom-1 -right-1">
-              <div className=" hover:bg-gray-200 shadow rounded-full w-7 h-7 bg-white flex items-center justify-center ">
-                <FaCamera />
-              </div>
-            </div>
-          </div>
-          <span className="text-xl font-bold w-max">
-            {userInfo.firstName} {userInfo.lastName}
-          </span>
-          <span className="text-sm font-bold rounded  bg-gray-100 px-6 py-2">
-            {userInfo.role}
-          </span>
-        </div>
-        <div className="w-full h-full px-4 py-1 flex flex-col space-y-6">
-          <h1 className="text-3xl">Données personnelles</h1>
+      <hr />
+    </div>
+  );
+};
 
-          <hr />
-          <div>
-            <strong className="font-normal">Nom : </strong>
-            <div className="text-gray-400">{userInfo.firstName}</div>
-          </div>
-          <div>
-            <strong className="font-normal">Prénom : </strong>
-            <div className="text-gray-400">{userInfo.lastName}</div>
-          </div>
-          <div>
-            <strong className="font-normal">Adresse email : </strong>
-            <div className="text-gray-400">{userInfo.email}</div>
-          </div>
-          <div>
-            <strong className="font-normal">N° téléphone : </strong>
-            <div className="text-gray-400">Aucun numéro ajouté.</div>
-          </div>
-          <div>
-            <strong className="font-normal">Rôle : </strong>
-            <div className="text-gray-400">{userInfo.role}</div>
-          </div>
+const ProfileCard = ({ firstName, lastName, role, className }) => (
+  <div
+    id="profile_card"
+    className={`flex flex-col border rounded py-2 px-10 h-max items-center w-full  space-y-6 ${className}`}
+  >
+    <div className="relative">
+      <div className="rounded-full w-20 h-20 overflow-hidden flex items-center justify-center">
+        <img
+          className="object-cover h-full w-full"
+          src="https://source.unsplash.com/random"
+        />
+      </div>
+      <div className="absolute rounded-full w-8 h-8 bg-white flex items-center justify-center -bottom-1 -right-1">
+        <div className=" hover:bg-gray-200 shadow rounded-full w-7 h-7 bg-white flex items-center justify-center ">
+          <FaCamera />
         </div>
       </div>
+    </div>
+    <span className="text-xl font-bold w-max">
+      {firstName} {lastName}
+    </span>
+    <span className="text-sm font-bold rounded  bg-gray-100 px-6 py-2">
+      {role}
+    </span>
+  </div>
+);
+const Info = ({ label, info, className }) => {
+  return (
+    <div className={className}>
+      <strong className="font-normal">{label} : </strong>
+      <div className="text-gray-400">{info}</div>
+    </div>
+  );
+};
+const Profile = ({ className }) => {
+  const { userInfo } = useAuth();
+
+  // useEffect(() => {
+  //   return database.users
+  //     .doc(user.uid)
+  //     .onSnapshot((doc) => setUserInfo({ ...doc.data() }));
+  // }, [user.uid]);
+
+  return (
+    <div
+      name="container"
+      className={
+        className +
+        ' grid grid-cols-1  md:grid-cols-2 w-full  gap-y-3 gap-x-6 p-6 '
+      }
+    >
+      <Title />
+
+      <ProfileCard
+        className="row-start-2 md:row-span-5 justify-self-center"
+        firstName={userInfo.firstName}
+        lastName={userInfo.lastName}
+        role={userInfo.role}
+      />
+
+      <Info label="Nom" info={userInfo.firstName} />
+      <Info label="Prénom" info={userInfo.lastName} />
+      <Info label="Adresse email" info={userInfo.email} />
+      <Info label="N° téléphone" info="Aucun numéro ajouté." />
+      <Info label="Rôle" info={userInfo.role} />
     </div>
   );
 };
