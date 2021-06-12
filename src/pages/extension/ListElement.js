4b1fbf129d5e39dc5b0c7b4c0ceeb08dcebe97a4
Alili <alili.mohamedlamine@gmail.com>
2021-06-12 20:08:25 +0100

    geocoder added but not functionnal

--- src/pages/extension/ListElement.js
+++ src/pages/extension/ListElement.js
@@ -4,54 +4,64 @@ import { VscGripper } from 'react-icons/vsc';
 import { FaPen, FaTrash, FaCircle } from 'react-icons/fa';
 import ListButton from './listButton';
 import classNames from 'classnames';
+import { reactRename } from 'react-rename';
+const DeleteButton = reactRename(ListButton, 'Deleting button');
+const EditButton = reactRename(ListButton, 'Edit button');
+const MoreButton = reactRename(ListButton, 'More about the project');
+
+const ProjectState = ({ state }) => (
+    <div className="flex items-center text-xs space-x-2">
+        <div>Etat:</div>
+        <div>
+            <FaCircle
+                className={classNames(
+                    {
+                        'text-red-400': state.bad,
+                    },
+                    {
+                        'text-yellow-400': state.medium,
+                    },
+                    { 'text-green-400': state.good }
+                )}
+            />
+        </div>
+        <div className="text-gray-400 ">{state.name}</div>
+    </div>
+);
 
 function ListElement({ name, description, state }) {
-  return (
-    <div className="flex flex-row  py-2 px-3 border border-gray-300 rounded">
-      <div className="flex flex-col self-start space-y-2">
-        <section>
-          <h1 className="text-2xl">{name}</h1>
-          <div className="text-gray-500 ">{description}</div>
-        </section>
+    return (
+        <div className="flex flex-row  py-2 px-3 border border-gray-300 rounded">
+            <div className="flex flex-col self-start space-y-2">
+                <section>
+                    <h1 className="text-2xl">{name}</h1>
+                    <div className="text-gray-500 ">{description}</div>
+                </section>
 
-        {/*icone + etat*/}
-        <div className="flex items-center text-xs space-x-2">
-          <div>Etat:</div>
-          <div>
-            <FaCircle
-              className={classNames(
-                {
-                  'text-red-400': state.bad,
-                },
-                {
-                  'text-yellow-400': state.medium,
-                },
-                { 'text-green-400': state.good }
-              )}
-            />
-          </div>
-          <div className="text-gray-400 ">{state.name}</div>
+                <ProjectState state={state} />
+            </div>
+
+            <div className="flex ml-auto space-x-5 self-end items-center self-center">
+                <EditButton
+                    name="modifier"
+                    className="border  text-xs hover:bg-gray-100"
+                >
+                    <FaPen />
+                </EditButton>
+
+                <DeleteButton
+                    name="supprimer"
+                    className="bg-red-500 hover:bg-red-700 text-white text-xs"
+                >
+                    <FaTrash />
+                </DeleteButton>
+
+                <MoreButton className="hover:bg-gray-100">
+                    <VscGripper />
+                </MoreButton>
+            </div>
         </div>
-      </div>
-
-      <div className="flex ml-auto space-x-5 self-end items-center self-center">
-        <ListButton name="modifier" className="border  text-xs">
-          <FaPen />
-        </ListButton>
-
-        <ListButton
-          name="supprimer"
-          className="bg-red-500 hover:bg-red-700 text-white text-xs"
-        >
-          <FaTrash />
-        </ListButton>
-
-        <button className="text-xl p-2 ml-3 border-gray-500 hover:bg-gray-200">
-          <VscGripper />
-        </button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default ListElement;
--- src/pages/extension/listButton.js
+++ src/pages/extension/listButton.js
@@ -3,12 +3,12 @@ import classNames from 'classnames';
 const standarStyle = 'flex self-stretch items-center space-x-2 p-2 rounded';
 
 const ListButton = ({ className, name, children, ...props }) => {
-  return (
-    <button className={classNames(standarStyle, className)} {...props}>
-      {children}
-      <div>{name}</div>
-    </button>
-  );
+    return (
+        <button className={classNames(standarStyle, className)} {...props}>
+            {children}
+            {name && <div>{name}</div>}
+        </button>
+    );
 };
 
 export default ListButton;
