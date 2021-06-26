6f99688aaa13609c3f52737ae3201ba41daef5a7
En3rg1z3r <feghouli.hamza@gmail.com>
2021-06-26 16:33:28 +0100

    got rid of hard coded garbage

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -4,18 +4,12 @@ import 'firebase/firestore';
 import 'firebase/storage';
 
 const app = firebase.initializeApp({
-    // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    // appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    apiKey: "AIzaSyBqyHR4wbBfzYXkKscdoSoVdAbp2FugbyA",
-    authDomain: "gestmap-de0e2.firebaseapp.com",
-    projectId: "gestmap-de0e2",
-    storageBucket: "gestmap-de0e2.appspot.com",
-    messagingSenderId: "711386265708",
-    appId: "1:711386265708:web:89761442871eeae7000049",
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
 const firestore = app.firestore();
