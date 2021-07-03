46af7a1eb67843a9a33ba4977018aa1fe3c064c8
Alili <alili.mohamedlamine@gmail.com>
2021-07-03 16:27:11 +0100

    Integration of AddProject component.

--- src/pages/dashboard/acceuil/acceuil.js
+++ src/pages/dashboard/acceuil/acceuil.js
@@ -1,9 +1,13 @@
 import { Redirect, Route, Switch } from 'react-router-dom';
+import IconButton from '../../../components/common/iconButton';
+import { FaPlus } from 'react-icons/fa';
 
 import CollectorTable from '../../projects/collectorTable';
 import ProjectTable from './projectTable';
+import { useHistory } from 'react-router';
 
 const Acceuil = () => {
+    const history = useHistory();
     return (
         <div className="space-y-6">
             {/* <TabsAcceuil /> */}
@@ -15,7 +19,15 @@ const Acceuil = () => {
                     <div className="text-3xl text-gray-500">
                         Liste des projets
                     </div>
-                    <ProjectTable />
+                    <div className="space-y-1">
+                        <ProjectTable />
+                        <IconButton
+                            className="bg-transparent shadow border w-full text-gray-500 rounded items-center justify-center"
+                            onClick={() => history.push('/ajouter')}
+                        >
+                            <FaPlus />
+                        </IconButton>
+                    </div>
                 </Route>
                 <Route path="/dashboard/acceuil/collecteurs">
                     <CollectorTable />
